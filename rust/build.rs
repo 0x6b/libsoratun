3d73cc3e447b6dbbc7dc836d664c01c40b407a1b
kaoru <k@warpnine.io>
2023-05-02 16:59:08 +0900

    build: macOS only for the moment

--- rust/build.rs
+++ rust/build.rs
@@ -1,7 +1,15 @@
 fn main() {
-    let path = "../lib/archive";
-    let lib = "soratun";
+    #[cfg(target_os = "macos")]
+    {
+        let path = "../lib/archive";
+        let lib = "soratun";
 
-    println!("cargo:rustc-link-search=native={}", path);
-    println!("cargo:rustc-link-lib=static={}", lib);
+        println!("cargo:rustc-link-search=native={}", path);
+        println!("cargo:rustc-link-lib=static={}", lib);
+    }
+
+    #[cfg(not(target_os = "macos"))]
+    {
+        todo!()
+    }
 }

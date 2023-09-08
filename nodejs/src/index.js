90b6f10bff3812ef2fec51f578ffc4931ed26083
kaoru <k@warpnine.io>
2023-09-08 09:43:19 +0900

    feat: add Node.js example

---
+++ nodejs/src/index.js
@@ -0,0 +1,11 @@
+const { Library } = require("ffi-napi");
+const { readFileSync } = require("fs");
+
+// if you are on macOS, you have to rename the library `libsoratun.so` to `libsoratun.dylib`,
+// since ffi-napi does not support `.so` extension on macOS.
+const soratun = Library("libsoratun", {
+  Send: ["string", ["string", "string", "string", "string"]],
+});
+
+const config = readFileSync(process.argv[2], "utf8");
+soratun.Send(config, "POST", "/", `{"message": "hello"}`);

421d049d4feca479bafd077c553901bf818ffc86
kaoru <k@warpnine.io>
2023-09-16 10:31:24 +0900

    docs: comments

--- libsoratun/client.go
+++ libsoratun/client.go
@@ -31,6 +31,15 @@ type Params struct {
 	Body string
 }
 
+// NewUnifiedEndpointHTTPClient creates a new HTTP client specific for the SORACOM Unified Endpoint.
+//
+// Args:
+// - `config`: A Config object containing the configuration for the SORACOM Arc connection.
+//
+// Returns:
+// - `*UnifiedEndpointHTTPClient`: A pointer to the created UnifiedEndpointHTTPClient and an error object. If there's any error occurred during setting up the tunnel connection or parsing the endpoint URL, the error will be returned.
+//
+// The created client uses the http.Transport with a custom DialContext for making HTTP requests. The User-Agent header for all requests made by this client is set to "libsoratun/0.0.1".
 func NewUnifiedEndpointHTTPClient(config Config) (*UnifiedEndpointHTTPClient, error) {
 	t, err := newTunnel(&config)
 	if err != nil {
@@ -53,6 +62,16 @@ func NewUnifiedEndpointHTTPClient(config Config) (*UnifiedEndpointHTTPClient, er
 	}, nil
 }
 
+// MakeRequest creates a new HTTP request based on provided parameters.
+//
+// Args:
+// - `params`: A Params struct containing the parameters for the request.
+//   - `params.Method`: The HTTP method of the request. Only GET or POST is supported. Any extra spaces are trimmed.
+//   - `params.Path`: The path of the request. It cannot be empty, and any leading slashes are removed.
+//   - `params.Body`: The body of the request. It cannot be empty.
+//
+// Returns:
+// - `*http.Request`: A pointer to the created http.Request, and an error object.
 func (c *UnifiedEndpointHTTPClient) MakeRequest(params *Params) (*http.Request, error) {
 	method := strings.TrimSpace(params.Method)
 	if !(method == http.MethodGet || method == http.MethodPost) {
@@ -84,6 +103,13 @@ func (c *UnifiedEndpointHTTPClient) MakeRequest(params *Params) (*http.Request, 
 	return req, nil
 }
 
+// DoRequest sends an HTTP request and returns the response.
+//
+// Args:
+// - `req`: A pointer to the http.Request object to be sent.
+//
+// Returns:
+// - `*http.Response`: A pointer to the http.Response, and an error object.
 func (c *UnifiedEndpointHTTPClient) DoRequest(req *http.Request) (*http.Response, error) {
 	res, err := c.httpClient.Do(req)
 	if err != nil {
